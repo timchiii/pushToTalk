713882f7f4cfa8ace5d5605d987cf92b9f5b964a
Tim Brock <phxxxx@gmail.com>
2021-10-22 18:31:43 -0700

    confirmed working

--- muteButton.ino
+++ muteButton.ino
@@ -1,14 +1,39 @@
-#include "Keyboard.h"
+/*
+    Wiring Diagram
+    GND -> 10kR 
+    GND -> LED-
+    10kR -> PIN 4
+    PIN 5 -> button bottom
+    VCC -> button side
+    VCC -> LED+
+    PIN 5 -> LED Data
 
-const int button = 4;
-const int led = 5;
+*/
+
+
+#include "Keyboard.h"
+#include "HardwareSerial.h"
+#include <FastLED.h>
+
+#define NUM_LEDS 24
+#define DATA_PIN 5
+#define BRIGHTNESS 200
+
+int button = 4;
 int lastState = HIGH;
+bool wasMuted = 1;
+
+CRGB leds[NUM_LEDS];
 
 void setup()
 {
     pinMode(button, INPUT);
-    pinMode(led, OUTPUT);
-    Keyboard.begin;
+    pinMode(DATA_PIN, OUTPUT);
+    Keyboard.begin();
+    Serial.begin(115200);
+    FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+    fill_solid(leds, NUM_LEDS, CRGB::Purple);
+    FastLED.show();
 }
 
 // LOW = pressed
@@ -16,39 +41,42 @@ void setup()
 
 void loop()
 {
+
     int nowState = digitalRead(button);
+    //Serial.println(String(lastState) + String(nowState));
 
     /* if button state has changed send
        CTRL-SHIFT-m to toggle mute */
-    if (nowState != lastState)
+    if (lastState != nowState)
     {
-        //press left ctrl
+        if (nowState == HIGH)
+        {
+            fill_solid(leds, NUM_LEDS, CRGB::Purple);
+            FastLED.show();
+            wasMuted = 0;
+            Serial.println(nowState);
+        }
+
+        if (nowState == LOW)
+        {
+            fill_solid(leds, NUM_LEDS, CRGB::Red);
+            FastLED.show();
+            wasMuted = 1;
+            Serial.println(nowState);
+        }
+
+        //toggle mute
         Keyboard.press(KEY_LEFT_CTRL);
-
-        // press left shift
         Keyboard.press(KEY_LEFT_SHIFT);
-
-        // press m
-        Keyboard.press('m')
-
+        Keyboard.press('m');
+        
         // pause briefly
         delay(100);
 
         // release all keys
         Keyboard.releaseAll();
-
-        if (nowState == HIGH)
-        {
-            // illuminate green led
-            digitalWrite(led, HIGH);
-        }
-        if (nowState == LOW)
-        {
-            // illuminate green led
-            digitalWrite(led, LOW);
-        }
     }
-    
+
     // set lastState to current state
     lastState = nowState;
 }
